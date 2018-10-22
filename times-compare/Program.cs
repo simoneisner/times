99c4898e83585717aa073726ca033232cfd6b20a
Simon Eisner <seisner@gmail.com>
2018-10-22 11:04:38 -0400

    Add project files.

---
+++ times-compare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace times_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
