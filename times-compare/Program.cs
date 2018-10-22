c7c5d188d8b09f9c3f6733f4f1b4ab34ef261421
Simon Eisner <seisner@gmail.com>
2018-10-22 12:18:00 -0400

    initial changes

--- times-compare/Program.cs
+++ times-compare/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using times_compare.Helpers;
 
 namespace times_compare
 {
@@ -6,7 +10,59 @@ namespace times_compare
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<DateRange> ListA = new List<DateRange>();
+            List<DateRange> ListB = new List<DateRange>();
+            List<DateRange> Difference = new List<DateRange>();
+
+            ListA.Add(new DateRange("9:00", "10:00"));
+            ListB.Add(new DateRange("9:00", "9:30"));
+
+            
+            foreach (DateRange drA in ListA)
+            {
+
+                foreach(DateRange drB in ListB)
+                {
+                    DateRange drDiff = new DateRange();
+                    DateTime diffStart = new DateTime();
+                    DateTime diffEnd = new DateTime();
+
+                    diffStart = drB.Start;
+                    if (drA.Start < drB.Start)
+                    {
+                        diffStart = drA.Start;
+                    }
+
+                    diffEnd = drB.End;
+                    if(drA.End > drB.End)
+                    {
+                        diffEnd = drA.End;
+                    }
+
+                    Difference.Add(new DateRange(diffStart.ToString(), diffEnd.ToString()));
+                }
+                
+            }
+
+
+            foreach(DateRange dr in Difference)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(dr.Start.Hour);
+                sb.Append(":");
+                sb.Append(dr.Start.Minute);
+                sb.Append(" - ");
+                sb.Append(dr.End.Hour);
+                sb.Append(":");
+                sb.Append(dr.End.Minute);
+                Console.WriteLine(sb.ToString());
+            }
+
+           
+
+            Console.ReadLine();
+            
+            
         }
     }
 }
